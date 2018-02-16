709e7c9367ec6b47568890d887348639205c2739
1amb3 <a403481704@gmail.com>
2018-02-16 23:28:12 +0800

    fix

--- gunicorn.py
+++ gunicorn.py
@@ -10,6 +10,6 @@ bind = 'unix:/tmp/wxchat.socket'
 backlog = 512                #监听队列
 timeout = 30      #超时
 worker_class = 'gevent' #使用gevent模式，还可以使用sync 模式，默认的是sync模式
-
+daemon=True
 workers = multiprocessing.cpu_count() * 2 + 1    #进程数
 threads = 2 #指定每个进程开启的线程数
