70f5e4e44960f489badf35bfd4d58cd903cc3714
1amb3 <a403481704@gmail.com>
2018-02-17 19:24:43 +0800

    add new feature develop

---
+++ README.md
@@ -0,0 +1,6 @@
+code for weixin
+
+```
+./start_gunicorn.sh to start gunicorn
+./close_gunicorn.sh to close gunicorn
+```
--- start_gunicorn.sh
+++ start_gunicorn.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ ! -d "logs" ]; then
+    mkdir logs
+fi
 echo "Start new gunicorn......"
 source venv/bin/activate
 gunicorn -c gunicorn.py wxchat:app
